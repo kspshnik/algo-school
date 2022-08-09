a302d18d155c0e256c46c3a97db80a544e9ad0bb
Evgeny Karpel <kspshnik@yandex.ru>
2022-08-09 22:10:42 +0300

    [Feature] reverseStringGenerator(); Генератор, реализующий переворот строки написан и протестирован.

---
+++ src/services/algorithms/reverse-string-generator.ts
@@ -0,0 +1,20 @@
+import { reverseStringStep } from './index';
+
+function* reverseStringGenerator(str: string) : Generator<string, string | void, never> {
+  if (str.length > 2) {
+    let head = 0;
+    let tail = str.length - 1;
+    let oldStr = str;
+    do {
+      const newStr = reverseStringStep(oldStr, head, tail);
+      yield newStr;
+      head += 1;
+      tail -= 1;
+      oldStr = newStr;
+    } while (head < tail);
+  } else {
+    yield str;
+  }
+}
+
+export default reverseStringGenerator;
--- src/services/algorithms/reverse-string-step.test.ts
+++ src/services/algorithms/reverse-string-step.test.ts
@@ -16,6 +16,7 @@ describe('testing reverse string-reverse-page step', () => {
     expect(reverseStringStep('ab', 0, 1)).toEqual('ba');
     expect(reverseStringStep('poke', 1, 2)).toEqual('pkoe');
     expect(reverseStringStep('alfabeta', 2, 5)).toEqual('aleabfta');
+    expect(reverseStringStep('cinema', 0, 5)).toEqual('ainemc');
   });
   it('should return a string-reverse-page itself if head === tail', () => {
     expect(reverseStringStep('react', 2, 2)).toEqual('react');
@@ -24,5 +25,6 @@ describe('testing reverse string-reverse-page step', () => {
     expect(reverseStringStep('ava', 0, 2)).toEqual('ava');
     expect(reverseStringStep('poker', 1, 3)).toEqual('pekor');
     expect(reverseStringStep('react', 1, 3)).toEqual('rcaet');
+    expect(reverseStringStep('oranges', 0, 6)).toEqual('srangeo');
   });
 });
