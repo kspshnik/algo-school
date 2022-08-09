28728468832ff1454c9d62a85f773a831c5030e5
Evgeny Karpel <kspshnik@yandex.ru>
2022-08-10 00:48:26 +0300

    [Refactor] Уточнение типизации

---
+++ src/constants/type-literals.ts
@@ -0,0 +1,2 @@
+export const BUBBLE_SORT = 'BUBBLE';
+export const INSERTION_SORT = 'INSERTION';
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,1 +1,3 @@
-export type TSortingType = 'BUBBLE' | 'INSERTION';
+import { BUBBLE_SORT, INSERTION_SORT } from '../constants/type-literals';
+
+export type TSortingType = typeof BUBBLE_SORT | typeof INSERTION_SORT;
