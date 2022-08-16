a87d1eaf8df55d469084af9f8b4c94a2613446e9
Evgeny Karpel <kspshnik@yandex.ru>
2022-08-16 17:29:42 +0300

    [Feature] Порядок сортировки пузырьком

--- src/services/algorithms/bubble-sorting-step-generator.ts
+++ src/services/algorithms/bubble-sorting-step-generator.ts
@@ -1,32 +1,39 @@
 import { TSortStepResult } from '../../types/algo-struct.types';
 
 import { swapItemsInArray } from '../helpers';
+import { Direction } from '../../types/direction';
 
 function* bubbleSortingStepGenerator(
   arr : Array<number>,
+  direction : Direction,
 ) : Generator<TSortStepResult, void, never> {
   const res : TSortStepResult = {
     baseItem: 0,
     currentItem: 0,
-    isSwitched: false,
     doneItems: [],
     value: [],
   };
-  const start = 0;
-  let end: number = arr.length - 1;
-  let base: number = start;
-  let current: number;
+  // const directionMultiplier = direction === Direction.Descending ? -1 : 1;
+  // const directionCorrector = direction === Direction.Ascending;
+  const [start, finish, step, corrector] = direction === Direction.Descending
+    ? [arr.length - 1, 0, -1, false]
+    : [0, arr.length - 1, 1, true];
+  //  let end : number = direction === Direction.Descending ? 0 : arr.length - 1;
+  let end : number = finish;
+  let base : number = start;
+  let current : number;
   let work : Array<number> = arr;
   const sorted : Array<number> = [];
   const isDone = () : boolean => work.length === sorted.length;
   while (!isDone()) {
-    for (base = start; base < end - 1; base += 1) {
-      current = base + 1;
+    for (base = start; (((end - base) * step - 1) > 0); base += step) {
+      current = base + step;
       res.baseItem = base;
       res.currentItem = current;
       res.value = work;
       yield res;
-      if (work[base] > work[current]) {
+      const criteria = corrector ? work[base] > work[current] : work[current] > work[base];
+      if (criteria) {
         work = swapItemsInArray(work, base, current);
         res.baseItem = base;
         res.currentItem = current;
@@ -35,7 +42,7 @@ function* bubbleSortingStepGenerator(
       }
     }
     sorted.push(end);
-    end += 1;
+    end -= 1;
     res.value = work;
     res.doneItems = sorted;
     yield res;
