6b22ec97af76e17803e9a356f5f047e067fe0f2e
Evgeny Karpel <kspshnik@yandex.ru>
2022-08-22 16:50:28 +0300

    [Interim] Передача на iMac

--- src/index.tsx
+++ src/index.tsx
@@ -9,32 +9,38 @@ import * as Sentry from '@sentry/react';
 import { BrowserTracing } from '@sentry/tracing';
 import App from './app/app';
 import { store } from './services/store';
-import ErrorPop
+// import ErrorPop
 
 Sentry.init({
-  dsn : 'https://5b896f5e6a29476ab5d97830abe3d544@o1127696.ingest.sentry.io/6631914',
-  integrations : [new BrowserTracing()],
-  debug : true,
-  attachStacktrace : true,
-  autoSessionTracking : true,
-  tracesSampleRate : 1.0,
+  dsn: 'https://5b896f5e6a29476ab5d97830abe3d544@o1127696.ingest.sentry.io/6631914',
+  integrations: [new BrowserTracing()],
+  debug: true,
+  attachStacktrace: true,
+  autoSessionTracking: true,
+  tracesSampleRate: 1.0,
 });
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Sentry.ErrorBoundary
-        fallback={({ error, componentStack, resetError }) => (
-          <ErrorPopup
-
-            <App />
-
-          </Provider>
-          </React.StrictMode>,
-          document.getElementById('root'),
+        fallback={({ error, componentStack, resetError }) => {
+          console.dir(error);
+          console.dir(componentStack);
+          return (
+            <div>
+              {error.toString()}
+            </div>
           );
+        }}>
+        <App />
+      </Sentry.ErrorBoundary>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
 
-          // If you want to start measuring performance in your app, pass a function
-          // to log results (for example: reportWebVitals(console.log))
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/services/helpers/get-element-state.ts
+++ src/services/helpers/get-element-state.ts
@@ -3,7 +3,8 @@ import { ElementStates } from '../../types/element-states';
 const getElementState = (isActive : boolean, isDone : boolean) : ElementStates => {
   let state : ElementStates;
   if (isActive && isDone) {
-    throw new Error("Item can't be changing and ready at the same time, sorry.");
+    state = ElementStates.Modified;
+    //   throw new Error("Item can't be changing and ready at the same time, sorry.");
   } else if (isActive && !isDone) {
     state = ElementStates.Changing;
   } else if (!isActive && isDone) {
