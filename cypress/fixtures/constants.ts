584bba4e6285ba1e26463a932f6fc09e82398736
Evgeny Karpel <kspshnik@yandex.ru>
2023-04-25 23:18:45 +0300

    [Feature] Cypress tests string page

---
+++ cypress/fixtures/constants.ts
@@ -0,0 +1,5 @@
+export const defaultColor = 'rgb(0, 50, 255)';
+export const changingColor = 'rgb(210, 82, 225)';
+export const modifiedColor = 'rgb(127, 224, 81)';
+export const circleMainClass = 'div[class^="circle_circle"]';
+export const circleContentClass = 'div[class^="circle_content"]';
