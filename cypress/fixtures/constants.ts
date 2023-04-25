ea75d9476791153e66380dfa15ea130eb9c885e4
Evgeny Karpel <kspshnik@yandex.ru>
2023-04-25 23:58:03 +0300

    [Feature] Cypress tests fibonacci page

--- cypress/fixtures/constants.ts
+++ cypress/fixtures/constants.ts
@@ -3,3 +3,4 @@ export const changingColor = 'rgb(210, 82, 225)';
 export const modifiedColor = 'rgb(127, 224, 81)';
 export const circleMainClass = 'div[class^="circle_circle"]';
 export const circleContentClass = 'div[class^="circle_content"]';
+export const fibonnachiSeq = [0, 1, 1, 2, 3, 5, 8, 13];
